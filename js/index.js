6de6bab27af71f445392aa9df20e9cba35f9c7b7
EtbonDeveloper <etbon.development@gmail.com>
2023-01-03 18:53:14 +0200

    First commit, end creating index page, add styles, js script logic for lessons

---
+++ js/index.js
@@ -0,0 +1,151 @@
+const timetable = {
+    1: {
+        '08:30': {
+	        'lesson': 'Технологія',
+	        'classroom_code': 'md3a47s',
+            'zoom_refs': 'Постійне посилання на zoom: <a href="https://us04web.zoom.us/j/3197524662?pwd=mNgDz72VTb68NXISNR5hfGcOCp12Tl.1">Натисни на мене, щоб перейти до zoom конференції.</a>'
+	    },
+        '10:00': {
+	        'lesson': 'Математика',
+	        'classroom_code': 'ikkjwrr',
+            'zoom_refs': 'Постійного посилання на конференцію для цього предмета немає! Заходьте за новим посиланням у classroom.'
+	    },
+        '11:30': {
+	        'lesson': 'Теорія кіл',
+	        'classroom_code': 'xbwv4zc',
+            'zoom_refs': 'Постійного посилання на конференцію для цього предмета немає! Заходьте за новим посиланням у classroom.'
+	    }
+    },
+    2: {
+        '09:00': {
+	        'lesson': 'Інженерна графіка',
+	        'classroom_code': 'idfnskr',
+            'zoom_refs': 'Постійного посилання на конференцію для цього предмета немає! Заходьте за новим посиланням у classroom.'
+	    },
+        '10:00': {
+	        'lesson': 'Математика',
+	        'classroom_code': 'ikkjwrr',
+            'zoom_refs': 'Постійного посилання на конференцію для цього предмета немає! Заходьте за новим посиланням у classroom.'
+	    },
+        '11:30': {
+	        'lesson': 'Фізика',
+	        'classroom_code': 'ioi25s6',
+            'zoom_refs': 'Постійного посилання на конференцію для цього предмета немає! Заходьте за новим посиланням у classroom.'
+	    },
+        '13:00': {
+	        'lesson': 'Фізчна культура',
+	        'classroom_code': '<a href="https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap">Натисни на мене, щоб перейти у classroom Фізичної культури.</a>',
+            'zoom_refs': 'Посилання на конференцію немає!'
+	    }
+    },
+    3: {
+        '08:30': {
+	        'lesson': 'Технологія',
+	        'classroom_code': 'md3a47s',
+            'zoom_refs': 'Постійне посилання на zoom: <a href="https://us04web.zoom.us/j/3197524662?pwd=mNgDz72VTb68NXISNR5hfGcOCp12Tl.1">Натисни на мене, щоб перейти до zoom конференції.</a>'
+	    },
+        '10:00': {
+	        'lesson': 'Культурологія',
+	        'classroom_code': 'ivwxte7',
+            'zoom_refs': 'Постійного посилання на конференцію для цього предмета немає! Заходьте за новим посиланням у classroom.'
+	    },
+        '11:30': {
+	        'lesson': 'Укр. мова та Укр. літ',
+	        'classroom_code': 'Укр. мова: zuwqrzq. Укр. літ: pgeaznn',
+            'zoom_refs': 'Постійне посилання на конференцію: <a href="http://meet.google.com/kgj-repw-nmh">Натисни на мене, щоб перейти до google meet конференції.</a>'
+	    }
+    },
+    4: {
+        '08:30': {
+	        'lesson': 'Англ. мова',
+	        'classroom_code': 'Пугачова: <a href="https://classroom.google.com/c/NTQ0OTY5NTkyNDgw?cjc=x4z7qq7">Натисни на мене, щоб перейти у classroom Англійської мови.</a>',
+            'zoom_refs': 'Постійного посилання на конференцію для цього предмета немає! Заходьте за новим посиланням у classroom.'
+	    },
+        '10:00': {
+	        'lesson': 'Інформатика',
+	        'classroom_code': 'tfg2ih5',
+            'zoom_refs': 'Постійного посилання на конференцію для цього предмета немає! Заходьте за новим посиланням у classroom.'
+	    },
+        '11:30': {
+	        'lesson': 'Біологія',
+	        'classroom_code': 'oeu36uo',
+            'zoom_refs': 'Постійне посилання zoom: <a href="https://us05web.zoom.us/j/8411452864?pwd=ZVRXUlFrQ3B6YVVKZUt3eVozMTU3UT09">Натисни на мене, щоб перейти до zoom конференції.</a>'
+	    },
+        '13:00': {
+	        'lesson': 'Укр. мова',
+	        'classroom_code': 'zuwqrzq',
+            'zoom_refs': 'Постійне посилання на конференцію: <a href="http://meet.google.com/kgj-repw-nmh">Натисни на мене, щоб перейти до google meet конференції.</a>'
+	    }
+    },
+    5: {
+        '08:30': {
+	        'lesson': 'Громадянська',
+	        'classroom_code': 'tlvmkja',
+            'zoom_refs': 'Постійне посилання zoom: <a href="https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09">Натисни на мене, щоб перейти до zoom конференції.</a>'
+	    },
+        '10:00': {
+	        'lesson': 'Історія',
+	        'classroom_code': 'x4wh3k7',
+            'zoom_refs': 'Discord: <a href="https://discord.gg/CWrxRjr6aT">Натисни на мене, щоб перейти до Discord сервера.</a>'
+	    },
+        '11:30': {
+	        'lesson': 'Захист Вітчизни',
+	        'classroom_code': 'npgncuz',
+            'zoom_refs': 'Постійне посилання на конференцію: <a href="https://meet.google.com/qag-kqao-znf">Натисни на мене, щоб перейти до google meet конференції.</a>'
+	    },
+        '13:00': {
+	        'lesson': 'Фізика',
+	        'classroom_code': 'ioi25s6',
+            'zoom_refs': 'Постійного посилання на конференцію для цього предмета немає! Заходьте за новим посиланням у classroom.'
+	    }
+    }
+}
+
+const daysDict = {1: 'Понеділок', 2: 'Вівторок', 3: 'Середа', 4: 'Четвер', 5: 'П\'ятниця'};
+
+document.querySelectorAll('.calls').forEach(btn => btn.onclick = () => location.href = '../rt-distance-website/pages/calls.html');
+document.querySelectorAll('.classes').forEach(btn => btn.onclick = () => location.href = '../rt-distance-website/pages/classes.html');
+document.querySelector('#refresh-lesson-info-btn').onclick = () => writeLessonInfoInTag(getLessonInfo(getLessonTime()));
+
+const getLessonTime = () => {
+    const hours = new Date().getHours();
+    const minutes = new Date().getMinutes();
+    let lessonTime;
+
+    if (hours >= 8 && minutes >= 30 && hours < 10)
+        lessonTime = '08:30';
+    else if (hours >= 11 && minutes >= 30 && hours < 13)
+        lessonTime = '11:30';
+    else if (hours >= 10 && hours < 13)
+        lessonTime = '10:00';
+    else if (((hours >= 13 && hours < 14) || (hours >= 14 && minutes < 30)) && hours < 15)
+        lessonTime = '13:00';
+
+    return lessonTime;
+};
+
+const getLessonInfo = lessonTime => {
+    const dayOfWeek = new Date().getDay();
+
+    try {
+        return timetable[dayOfWeek][lessonTime];
+    } catch {
+        return undefined;
+    }
+};
+
+const writeLessonInfoInTag = lessonInfo => {
+    const htmlLessonInfoParagraph = document.getElementById('lesson-info');
+    const currentTime = new Date().toLocaleTimeString();
+    if (lessonInfo) {
+        htmlLessonInfoParagraph.innerHTML = `${daysDict[new Date().getDay()]}:<br>
+        Сьогодні о ${currentTime} у нас за розкладом: ${lessonInfo['lesson']}.<br>
+        Код або посилання classroom: ${lessonInfo['classroom_code']}<br>
+        ${lessonInfo['zoom_refs']}`;
+    } else
+        htmlLessonInfoParagraph.innerHTML = `Зараз: ${currentTime}. На сьогодні пари вже закінчилися, або сьогодні вихідний, відпочивайте!`;
+};
+
+writeLessonInfoInTag(getLessonInfo(getLessonTime()));
+
+setTimeout(() => document.querySelectorAll('a').forEach(el => el.setAttribute('target', '_blank')), 0);
