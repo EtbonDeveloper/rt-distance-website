c05bafe5a518dd2430849d7b4339b90e11aaad3d
EtbonDeveloper <etbon.development@gmail.com>
2023-09-23 23:57:41 +0300

    add switching classroom codes to links logic

--- js/classroom-codes.js
+++ js/classroom-codes.js
@@ -1,20 +1,66 @@
 import schedule from './schedule.js';
 
 const codesList = document.querySelector('#classroom-codes');
+const classroomButton = document.querySelector('#classroom-btn');
 
-codesList.innerHTML = '';
-const provenProperties = [];
+classroomButton.addEventListener('click', function () {
+    if (this.className == 'codes') {
+        this.className = 'links';
+        this.innerText = 'Конвертувати у коди';
+    } else {
+        this.innerText = 'Конвертувати у посилання';
+        this.className = 'codes';
+    }
 
-for (const dayOfWeek in schedule) {
-    for (const lessonNumber in schedule[dayOfWeek]) {
-        if (Number.isInteger(+lessonNumber)) {
-            const lessonObject = schedule[dayOfWeek][lessonNumber]; 
-            const lessonName = lessonObject.lessonName;
-            const classroomCode = lessonObject.classroomCode;
-    
-            if (!provenProperties.includes(lessonName) && !lessonName.includes(' або ')) {
-                codesList.innerHTML += `<li>${lessonName}: ${classroomCode}</li>`;
-                provenProperties.push(lessonName);
+    writeClassroomCodes();
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    const className = localStorage.getItem('className');
+    const buttonText = localStorage.getItem('buttonText');
+
+    if (className && buttonText) {
+        classroomButton.className = className;
+        classroomButton.innerText = buttonText;
+    }
+
+    writeClassroomCodes();
+});
+
+window.addEventListener('beforeunload', () => {
+    localStorage.setItem('className', classroomButton.className);
+    localStorage.setItem('buttonText', classroomButton.innerText);
+});
+
+function writeClassroomCodes() {
+    codesList.innerHTML = '';
+    const provenProperties = [];
+
+    for (const dayOfWeek in schedule) {
+        for (const lessonNumber in schedule[dayOfWeek]) {
+            if (Number.isInteger(+lessonNumber)) {
+                const lessonObject = schedule[dayOfWeek][lessonNumber];
+                const lessonName = lessonObject.lessonName;
+                let classroomCode = lessonObject.classroomCode;
+
+                if (!provenProperties.includes(lessonName) && !lessonName.includes(' або ')) {
+                    const classroomCodeItem = document.createElement('li');
+
+                    if (classroomButton.className == 'codes') {
+                        const matches = [...classroomCode.matchAll(/cjc=([A-Za-z0-9]{7})/g)];
+                        const classroomCodes = [];
+
+                        for (const match of matches) {
+                            classroomCodes.push(match[1]);
+                        }
+
+                        classroomCode = [...new Set(classroomCodes)].join(', ');
+                    }
+
+                    classroomCodeItem.innerHTML = `${lessonName}: ${classroomCode}`;
+                    codesList.prepend(classroomCodeItem);
+                    provenProperties.push(lessonName);
+                }
             }
         }
     }
--- js/schedule.js
+++ js/schedule.js
@@ -1,109 +1,109 @@
 const schedule = {
-    1: {
+	1: {
 		'strDayOfWeek': 'Понеділок',
 		'numberOfLessons': 4,
-        1: {
-	        'lessonName': 'Електронні пристрої та мікроелектроніка',
-	        'classroomCode': 'xi3dsg7',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-	    2: {
-	        'lessonName': 'Основи філософських знань',
-	        'classroomCode': '5h6rdgx',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-        3: {
-	        'lessonName': 'Фізичне виховання',
-	        'classroomCode': '<a href="https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap" target="_blank">натисни, щоб перейти за посиланням</a>',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-		4: {
-	        'lessonName': 'Джерела електроживлення РЕА',
-	        'classroomCode': 'mdnauxz',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    }
-    },
-    2: {
-		'strDayOfWeek': 'Вівторок',
-		'numberOfLessons': 3,
-        2: {
-	        'lessonName': 'Матеріалознавство радіоелектронних засобів та елементна база РЕА',
-	        'classroomCode': 'xpuxvha',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-        3: {
-	        'lessonName': 'Метрологія та вимірювальна техніка',
-	        'classroomCode': 'unrpbjs',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-        4: {
-	        'lessonName': 'Вища математика або Сигнали та процеси в радіотехніці',
-	        'classroomCode': 'jm4muh6, rmuq5oo',
-            'lessonLink': '<a href="https://us02web.zoom.us/j/3796839976?pwd=dEpTaERyUFFMQVJPTEcwVk1oLy8wZz09" target="_blank">натисни, щоб перейти за посиланням</a>, постійного посилання на конференцію для цього предмета немає!'
-	    }
-    },
-    3: {
-		'strDayOfWeek': 'Середа',
-		'numberOfLessons': 3,
-        2: {
-	        'lessonName': 'Іноземна мова (Даниленко та Приступа)',
-	        'classroomCode': 'Даниленко: soyolf5, Приступа: wxuz52i',
-            'lessonLink': 'Даниленко: постійного посилання на конференцію для цього предмета немає! Приступа: пара проходить у discord сервері!'
-	    },
-        3: {
-	        'lessonName': 'Фізика',
-	        'classroomCode': '<a href="https://classroom.google.com/c/NjIwODYwNzY3ODIy?hl=ru&hl=ru&cjc=y22aoi6" target="_blank">натисни, щоб перейти за посиланням</a>',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-	    4: {
-		    'lessonName': 'Електронні пристрої та мікроелектроніка або Метрологія та вимірювальна техніка',
-	        'classroomCode': 'xi3dsg7, unrpbjs',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    }
-    },
-    4: {
-		'strDayOfWeek': 'Четвер',
-		'numberOfLessons': 4,
-        1: {
-	        'lessonName': 'Сигнали та процеси в радіотехніці',
-	        'classroomCode': 'rmuq5oo',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-        2: {
-	        'lessonName': 'Радіопередавальні пристрої',
-	        'classroomCode': '7tgus3v',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-        3: {
-	        'lessonName': 'Фізичне виховання',
-	        'classroomCode': '<a href="https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap" target="_blank">Натисни, щоб перейти за посиланням.</a>',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-        4: {
-	        'lessonName': 'Основи технічної механіки',
-	        'classroomCode': '<a href="https://classroom.google.com/c/NjIwODA3OTk3ODc4?cjc=wixnvsa" target="_blank">натисни, щоб перейти за посиланням</a>',
-            'lessonLink': '<a href="https://us04web.zoom.us/j/2537342491?pwd=U2x0RU14Z01pdmFsVlBnckpGUWpudz09" target="_blank">натисни, щоб перейти за посиланням</a>'
-	    }
-    },
-    5: {
-		'strDayOfWeek': 'П\'ятниця',
-		'numberOfLessons': 3,
-        2: {
-			'lessonName': 'Вища математика',
-	        'classroomCode': 'jm4muh6',
-            'lessonLink': '<a href="https://us02web.zoom.us/j/3796839976?pwd=dEpTaERyUFFMQVJPTEcwVk1oLy8wZz09" target="_blank">натисни, щоб перейти за посиланням</a>'
-	    },
-        3: {
-			'lessonName': 'Матеріалознавство радіоелектронних засобів та елементна база РЕА',
-	        'classroomCode': 'xpuxvha',
-            'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
-	    },
-		4: {
-			'lessonName': 'Радіопередавальні пристрої',
-			'classroomCode': '7tgus3v',
+		1: {
+			'lessonName': 'Електронні пристрої та мікроелектроніка',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwODA2OTQ5NzIy?cjc=xi3dsg7" target="_blank">https://classroom.google.com/c/NjIwODA2OTQ5NzIy?cjc=xi3dsg7</a>',
 			'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
 		},
-    }
+		2: {
+			'lessonName': 'Основи філософських знань',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwODQ4NzkyOTMz?cjc=5h6rdgx" target="_blank">https://classroom.google.com/c/NjIwODQ4NzkyOTMz?cjc=5h6rdgx</a>',
+			'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
+		},
+		3: {
+			'lessonName': 'Фізичне виховання',
+			'classroomCode': '<a href="https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap" target="_blank">https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap</a>',
+			'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
+		},
+		4: {
+			'lessonName': 'Джерела електроживлення РЕА',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwODA4NTE0MDMw?cjc=mdnauxz" target="_blank">https://classroom.google.com/c/NjIwODA4NTE0MDMw?cjc=mdnauxz</a>',
+			'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
+		}
+	},
+	2: {
+		'strDayOfWeek': 'Вівторок',
+		'numberOfLessons': 3,
+		2: {
+			'lessonName': 'Матеріалознавство радіоелектронних засобів та елементна база РЕА',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwODA2NzYxMDkx?cjc=xpuxvha" target="_blank">https://classroom.google.com/c/NjIwODA2NzYxMDkx?cjc=xpuxvha</a>',
+			'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
+		},
+		3: {
+			'lessonName': 'Метрологія та вимірювальна техніка',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwNjM3NDE1NTYx?cjc=unrpbjs" target="_blank">https://classroom.google.com/c/NjIwNjM3NDE1NTYx?cjc=unrpbjs</a>',
+			'lessonLink': 'пара проходить у групі в Viber!'
+		},
+		4: {
+			'lessonName': 'Вища математика або Сигнали та процеси в радіотехніці',
+			'classroomCode': 'Вища математика: <a href="https://classroom.google.com/c/NjIwMTk3NzgwNzkw?cjc=jm4muh6" target="_blank">https://classroom.google.com/c/NjIwMTk3NzgwNzkw?cjc=jm4muh6</a><br>Сигнали та процеси в радіотехніці: <a href="https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo" target="_blank">https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo</a>',
+			'lessonLink': 'Вища математика: <a href="https://us02web.zoom.us/j/3796839976?pwd=dEpTaERyUFFMQVJPTEcwVk1oLy8wZz09" target="_blank">https://us02web.zoom.us/j/3796839976?pwd=dEpTaERyUFFMQVJPTEcwVk1oLy8wZz09</a><br>Сигнали та процеси в радіотехніці: пара проходить у групі в Viber!'
+		}
+	},
+	3: {
+		'strDayOfWeek': 'Середа',
+		'numberOfLessons': 3,
+		2: {
+			'lessonName': 'Іноземна мова (Даниленко та Приступа)',
+			'classroomCode': 'Даниленко: <a href="https://classroom.google.com/c/NjIwNjEyOTk5Nzc5?cjc=soyolf5" target="_blank">https://classroom.google.com/c/NjIwNjEyOTk5Nzc5?cjc=soyolf5</a><br>Приступа: <a href="https://classroom.google.com/c/NjIwNTE5NDc2Njgw?cjc=wxuz52i" target="_blank">https://classroom.google.com/c/NjIwNTE5NDc2Njgw?cjc=wxuz52i</a>',
+			'lessonLink': 'Даниленко: <a href="https://us05web.zoom.us/j/4928111409?pwd=YVIxOFdXemhRTGRjbWJpQzg1OFVhdz09" target="_blank">https://us05web.zoom.us/j/4928111409?pwd=YVIxOFdXemhRTGRjbWJpQzg1OFVhdz09</a><br>Приступа: пара проходить у discord сервері!'
+		},
+		3: {
+			'lessonName': 'Фізика',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwODYwNzY3ODIy?hl=ru&hl=ru&cjc=y22aoi6" target="_blank">https://classroom.google.com/c/NjIwODYwNzY3ODIy?hl=ru&hl=ru&cjc=y22aoi6</a>',
+			'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
+		},
+		4: {
+			'lessonName': 'Електронні пристрої та мікроелектроніка або Метрологія та вимірювальна техніка',
+			'classroomCode': 'Електронні пристрої та мікроелектроніка: <a href="https://classroom.google.com/c/NjIwODA2OTQ5NzIy?cjc=xi3dsg7" target="_blank">https://classroom.google.com/c/NjIwODA2OTQ5NzIy?cjc=xi3dsg7</a><br>Метрологія та вимірювальна техніка: <a href="https://classroom.google.com/c/NjIwNjM3NDE1NTYx?cjc=unrpbjs" target="_blank">https://classroom.google.com/c/NjIwNjM3NDE1NTYx?cjc=unrpbjs</a>',
+			'lessonLink': 'Електронні пристрої та мікроелектроніка: постійного посилання на конференцію для цього предмета немає!<br>Метрологія та вимірювальна техніка: пара проходить у групі в Viber!'
+		}
+	},
+	4: {
+		'strDayOfWeek': 'Четвер',
+		'numberOfLessons': 4,
+		1: {
+			'lessonName': 'Сигнали та процеси в радіотехніці',
+			'classroomCode': '<a href="https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo" target="_blank">https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo</a>',
+			'lessonLink': 'пара проходить у групі в Viber!'
+		},
+		2: {
+			'lessonName': 'Радіопередавальні пристрої',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwNjQyMDM0NjUz?cjc=7tgus3v" target="_blank">https://classroom.google.com/c/NjIwNjQyMDM0NjUz?cjc=7tgus3v</a>',
+			'lessonLink': 'пара проходить у групі в Viber!'
+		},
+		3: {
+			'lessonName': 'Фізичне виховання',
+			'classroomCode': '<a href="https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap" target="_blank">https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap</a>',
+			'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
+		},
+		4: {
+			'lessonName': 'Основи технічної механіки',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwODA3OTk3ODc4?cjc=wixnvsa" target="_blank">https://classroom.google.com/c/NjIwODA3OTk3ODc4?cjc=wixnvsa</a>',
+			'lessonLink': '<a href="https://us04web.zoom.us/j/2537342491?pwd=U2x0RU14Z01pdmFsVlBnckpGUWpudz09" target="_blank">https://us04web.zoom.us/j/2537342491?pwd=U2x0RU14Z01pdmFsVlBnckpGUWpudz09</a>'
+		}
+	},
+	5: {
+		'strDayOfWeek': 'П\'ятниця',
+		'numberOfLessons': 3,
+		2: {
+			'lessonName': 'Вища математика',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwMTk3NzgwNzkw?cjc=jm4muh6" target="_blank">https://classroom.google.com/c/NjIwMTk3NzgwNzkw?cjc=jm4muh6</a>',
+			'lessonLink': '<a href="https://us02web.zoom.us/j/3796839976?pwd=dEpTaERyUFFMQVJPTEcwVk1oLy8wZz09" target="_blank">https://us02web.zoom.us/j/3796839976?pwd=dEpTaERyUFFMQVJPTEcwVk1oLy8wZz09</a>'
+		},
+		3: {
+			'lessonName': 'Матеріалознавство радіоелектронних засобів та елементна база РЕА',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwODA2NzYxMDkx?cjc=xpuxvha" target="_blank">https://classroom.google.com/c/NjIwODA2NzYxMDkx?cjc=xpuxvha</a>',
+			'lessonLink': 'постійного посилання на конференцію для цього предмета немає!'
+		},
+		4: {
+			'lessonName': 'Радіопередавальні пристрої',
+			'classroomCode': '<a href="https://classroom.google.com/c/NjIwNjQyMDM0NjUz?cjc=7tgus3v" target="_blank">https://classroom.google.com/c/NjIwNjQyMDM0NjUz?cjc=7tgus3v</a>',
+			'lessonLink': 'пара проходить у групі в Viber!'
+		},
+	}
 }
 
 export default schedule;
