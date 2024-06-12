2030b7bac48c8cb9c98827ec1fa438af975293ad
EtbonDeveloper <etbon.development@gmail.com>
2024-06-12 13:12:18 +0300

    another attempt to correct the error for people abroad

--- js/current-lesson.js
+++ js/current-lesson.js
@@ -10,16 +10,15 @@ setInterval(writeLessonInfo, 1000);
 
 function writeLessonInfo() {
     const date = new Date();
-    const currentUADate = new Intl.DateTimeFormat('ua', {timeZone: 'Europe/Kyiv', weekday: 'short', hour: '2-digit', minute: '2-digit', second: '2-digit'}).formatToParts(date);
-    
-    const currentUATime = `${currentUADate.find(part => part.type === 'hour').value}:${currentUADate.find(part => part.type === 'minute').value}:${currentUADate.find(part => part.type === 'second').value}`;
-    const dayOfWeek = dayOfWeekObject[currentUADate.find(part => part.type === 'weekday').value];
-    
+    const currentUATime = new Intl.DateTimeFormat('ua', {timeZone: 'Europe/Kyiv', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false}).format(date);
+    const dayOfWeek = dayOfWeekObject[new Intl.DateTimeFormat('ua', {timeZone: 'Europe/Kyiv', weekday: 'short'}).format(date)];
+
     let lessonNumber = null;
 
     for (const call in calls) {
         if (currentUATime >= calls[call].startTime && currentUATime <= calls[call].endTime) {
             lessonNumber = call;
+            break;
         }
     }
 
@@ -36,9 +35,9 @@ function writeLessonInfo() {
     }
 
     if (lessonObject) {
-	    const lessonName = lessonObject.lessonName;
-	    const courseLink = lessonObject.courseLink;
-	    const lessonLink = lessonObject.lessonLink;
+        const lessonName = lessonObject.lessonName;
+        const courseLink = lessonObject.courseLink;
+        const lessonLink = lessonObject.lessonLink;
         lessonInfo.innerHTML = `Сьогодні ${strDayOfWeek}, зараз в ${currentUATime} у нас за розкладом: ${lessonName}<br>Посилання на курс: ${courseLink}<br>Постійне посилання на конференцію: ${lessonLink}`;
     } else if (dayOfWeek === dayOfWeekObject['сб'] || dayOfWeek === dayOfWeekObject['вс']) {
         lessonInfo.innerText = `Зараз: ${currentUATime}. Сьогодні вихідний, відпочивайте!`;
