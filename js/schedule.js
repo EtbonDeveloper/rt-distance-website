e0f00442ab8ca967ab1f106c8c9f200688ffc77f
EtbonDeveloper <etbon.development@gmail.com>
2024-09-02 10:06:22 +0300

    Change schedule information

--- js/schedule.js
+++ js/schedule.js
@@ -1,97 +1,87 @@
 const schedule = {
 	1: {
-		1: {
-			'lessonName': 'Пристрої НВЧ та антени',
+		2: {
+			'lessonName': 'РТС',
 			'courseLink': '<a href="https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo" target="_blank">https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo</a>',
 			'lessonLink': '<a href="https://meet.google.com/icp-hqmi-non" target="_blank">https://meet.google.com/icp-hqmi-non</a>'
 		},
-		2: {
-			'lessonName': 'Політологія',
-			'courseLink': '<a href="https://classroom.google.com/c/NjUzNTM0Mzg1Mjc0?cjc=wukrser" target="_blank">https://classroom.google.com/c/NjUzNTM0Mzg1Mjc0?cjc=wukrser</a>',
-			'lessonLink': 'немає'
-		},
 		3: {
-			'lessonName': 'Фізичне виховання',
-			'courseLink': '<a href="https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap" target="_blank">https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap</a>',
-			'lessonLink': 'немає'
+			'lessonName': 'невідомо',
+			'courseLink': 'невідомо',
+			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
+		},
+		4: {
+			'lessonName': 'невідомо/невідомо',
+			'courseLink': 'невідомо/невідомо',
+			'lessonLink': 'невідомо/невідомо'
 		}
 	},
 	2: {
 		1: {
-			'lessonName': 'Основи конструювання і технології виробництва РЕА',
-			'courseLink': '<a href="https://classroom.google.com/c/NjU0MTMzODk2Mjc2?cjc=65sm6o5" target="_blank">https://classroom.google.com/c/NjU0MTMzODk2Mjc2?cjc=65sm6o5</a>',
-			'lessonLink': 'немає'
-		},
-		2: {
-			'lessonName': 'Схемотехніка радіотехнічних пристроїв',
-			'courseLink': '<a href="https://distosvitart.gnomio.com/course/view.php?id=16" target="_blank">https://distosvitart.gnomio.com/course/view.php?id=16</a>',
-			'lessonLink': '<a href="https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09" target="_blank">https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09</a>'
-		},
-		3: {
-			'lessonName': 'ЕОМ та мікропроцесори',
-			'courseLink': '<a href="https://distosvitart.gnomio.com/course/view.php?id=17" target="_blank">https://distosvitart.gnomio.com/course/view.php?id=17</a>',
-			'lessonLink': '<a href="https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09" target="_blank">https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09</a>'
-		},
-		4: {
-			'lessonName': 'Радіоприймальні пристрої/Пристрої НВЧ та антени',
-			'courseLink': 'Радіоприймальні пристрої: <a href="https://classroom.google.com/c/NjA2OTgxNjYwNjAw?cjc=siqxbxg" target="_blank">https://classroom.google.com/c/NjA2OTgxNjYwNjAw?cjc=siqxbxg</a><br>Пристрої НВЧ та антени: <a href="https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo" target="_blank">https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo</a>',
-			'lessonLink': 'Радіоприймальні пристрої: <a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a><br>Пристрої НВЧ та антени: <a href="https://meet.google.com/icp-hqmi-non" target="_blank">https://meet.google.com/icp-hqmi-non</a>'
-		}
-	},
-	3: {
-		1: {
-			'lessonName': 'Радіоприймальні пристрої',
-			'courseLink': '<a href="https://classroom.google.com/c/NjA2OTgxNjYwNjAw?cjc=siqxbxg" target="_blank">https://classroom.google.com/c/NjA2OTgxNjYwNjAw?cjc=siqxbxg</a>',
+			'lessonName': 'невідомо',
+			'courseLink': 'невідомо',
 			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
 		},
 		2: {
-			'lessonName': 'Іноземна мова (Даниленко та Приступа)',
-			'courseLink': 'Даниленко: <a href="https://classroom.google.com/c/NjIwNjEyOTk5Nzc5?cjc=soyolf5" target="_blank">https://classroom.google.com/c/NjIwNjEyOTk5Nzc5?cjc=soyolf5</a><br>Приступа: <a href="https://classroom.google.com/c/NjIwNTE5NDc2Njgw?cjc=wxuz52i" target="_blank">https://classroom.google.com/c/NjIwNTE5NDc2Njgw?cjc=wxuz52i</a>',
-			'lessonLink': 'Даниленко: <a href="https://us05web.zoom.us/j/4928111409?pwd=YVIxOFdXemhRTGRjbWJpQzg1OFVhdz09" target="_blank">https://us05web.zoom.us/j/4928111409?pwd=YVIxOFdXemhRTGRjbWJpQzg1OFVhdz09</a><br>Приступа: пара проходить у discord сервері!'
+			'lessonName': 'невідомо',
+			'courseLink': 'невідомо',
+			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
 		},
 		3: {
-			'lessonName': 'ЕОМ та мікропроцесори/Основи автоматики',
-			'courseLink': 'ЕОМ та мікропроцесори: <a href="https://distosvitart.gnomio.com/course/view.php?id=17" target="_blank">https://distosvitart.gnomio.com/course/view.php?id=17</a><br>Основи автоматики: <a href="https://classroom.google.com/c/MTU2ODExNDMzNDMx?cjc=gbdpesb" target="_blank">https://classroom.google.com/c/MTU2ODExNDMzNDMx?cjc=gbdpesb</a>',
-			'lessonLink': 'ЕОМ та мікропроцесори: <a href="https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09" target="_blank">https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09</a><br>Основи автоматики: <a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
+			'lessonName': 'Економіка',
+			'courseLink': '<a href="https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja" target="_blank">https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja</a>',
+			'lessonLink': '<a href="https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09" target="_blank">https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09</a>'
+		}
+	},
+	3: {
+		2: {
+			'lessonName': 'Соціальна психологія',
+			'courseLink': '<a href="https://classroom.google.com/c/NzEwMTM5NTY1ODgx?cjc=3u6qgox" target="_blank">https://classroom.google.com/c/NzEwMTM5NTY1ODgx?cjc=3u6qgox</a>',
+			'lessonLink': 'невідомо'
+		},
+		3: {
+			'lessonName': 'РТС',
+			'courseLink': '<a href="https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo" target="_blank">https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo</a>',
+			'lessonLink': '<a href="https://meet.google.com/icp-hqmi-non" target="_blank">https://meet.google.com/icp-hqmi-non</a>'
 		},
 		4: {
-			'lessonName': 'Соціологія',
-			'courseLink': '<a href="https://classroom.google.com/c/NjUzNTM0MDY4NzI5?cjc=ousap2r" target="_blank">https://classroom.google.com/c/NjUzNTM0MDY4NzI5?cjc=ousap2r</a>',
-			'lessonLink': 'немає'
+			'lessonName': 'невідомо',
+			'courseLink': 'невідомо',
+			'lessonLink': 'невідомо'
 		}
 	},
 	4: {
 		1: {
-			'lessonName': 'Основи конструювання і технології виробництва РЕА/Основи телебачення',
-			'courseLink': 'Основи конструювання і технології виробництва РЕА: <a href="https://classroom.google.com/c/NjU0MTMzODk2Mjc2?cjc=65sm6o5" target="_blank">https://classroom.google.com/c/NjU0MTMzODk2Mjc2?cjc=65sm6o5</a><br>Основи телебачення: <a href="https://classroom.google.com/c/MTU2ODExNDMzNDQz?cjc=vo75pbq" target="_blank">https://classroom.google.com/c/MTU2ODExNDMzNDQz?cjc=vo75pbq</a>',
-			'lessonLink': 'Основи конструювання і технології виробництва РЕА: немає<br>Основи телебачення: <a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
-		},
-		2: {
-			'lessonName': 'Основи автоматики',
-			'courseLink': '<a href="https://classroom.google.com/c/MTU2ODExNDMzNDMx?cjc=gbdpesb" target="_blank">https://classroom.google.com/c/MTU2ODExNDMzNDMx?cjc=gbdpesb</a>',
+			'lessonName': 'невідомо/невідомо',
+			'courseLink': 'невідомо/невідомо',
 			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
 		},
+		2: {
+			'lessonName': 'невідомо',
+			'courseLink': 'невідомо',
+			'lessonLink': '<a href="https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09" target="_blank">https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09</a>'
+		},
 		3: {
-			'lessonName': 'Фізичне виховання',
-			'courseLink': '<a href="https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap" target="_blank">https://classroom.google.com/c/MTUxNTcxNTg1Nzkx?cjc=qgcrsap</a>',
-			'lessonLink': 'немає'
+			'lessonName': 'Економіка',
+			'courseLink': '<a href="https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja" target="_blank">https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja</a>',
+			'lessonLink': '<a href="https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09" target="_blank">https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09</a>'
 		}
 	},
 	5: {
-		1: {
-			'lessonName': 'Схемотехніка радіотехнічних пристроїв',
-			'courseLink': '<a href="https://distosvitart.gnomio.com/course/view.php?id=16" target="_blank">https://distosvitart.gnomio.com/course/view.php?id=16</a>',
-			'lessonLink': '<a href="https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09" target="_blank">https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09</a>'
-		},
 		2: {
-			'lessonName': 'Основи підприємництва',
-			'courseLink': '<a href="https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja" target="_blank">https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja</a>',
-			'lessonLink': '<a href="https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09" target="_blank">https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09</a>'
+			'lessonName': 'невідомо',
+			'courseLink': 'невідомо',
+			'lessonLink': 'невідомо'
 		},
 		3: {
-			'lessonName': 'Основи телебачення',
-			'courseLink': '<a href="https://classroom.google.com/c/MTU2ODExNDMzNDQz?cjc=vo75pbq" target="_blank">https://classroom.google.com/c/MTU2ODExNDMzNDQz?cjc=vo75pbq</a>',
-			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
+			'lessonName': 'невідомо',
+			'courseLink': 'невідомо',
+			'lessonLink': 'невідомо'
+		},
+		4: {
+			'lessonName': 'невідомо/невідомо',
+			'courseLink': 'невідомо/невідомо',
+			'lessonLink': 'невідомо/невідомо'
 		}
 	}
 }
