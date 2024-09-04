cf9a962d51f8e9bc809c90b22c8bde4297b6d085
EtbonDeveloper <etbon.development@gmail.com>
2024-09-04 16:56:55 +0300

    add new info to schedule

--- js/schedule.js
+++ js/schedule.js
@@ -1,35 +1,35 @@
 const schedule = {
 	1: {
 		2: {
-			'lessonName': 'РТС',
-			'courseLink': '<a href="https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo" target="_blank">https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo</a>',
+			'lessonName': 'Радіотехнічні та радіоелектронні системи',
+			'courseLink': '<a href="https://classroom.google.com/c/NzEwMzU2OTI0MjQ2?cjc=dhetudz" target="_blank">https://classroom.google.com/c/NzEwMzU2OTI0MjQ2?cjc=dhetudz</a>',
 			'lessonLink': '<a href="https://meet.google.com/icp-hqmi-non" target="_blank">https://meet.google.com/icp-hqmi-non</a>'
 		},
 		3: {
-			'lessonName': 'невідомо',
+			'lessonName': 'Апаратура систем зв\'язку та телебачення',
 			'courseLink': 'невідомо',
 			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
 		},
 		4: {
-			'lessonName': 'невідомо/невідомо',
+			'lessonName': 'Основи комп\'ютерного проектування РЕА/Конструювання ВЕТ і САПР',
 			'courseLink': 'невідомо/невідомо',
 			'lessonLink': 'невідомо/невідомо'
 		}
 	},
 	2: {
 		1: {
-			'lessonName': 'невідомо',
+			'lessonName': 'Контроль якості РЕА',
 			'courseLink': 'невідомо',
 			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
 		},
 		2: {
-			'lessonName': 'невідомо',
+			'lessonName': 'Основи ремонту та регулювання РЕА',
 			'courseLink': 'невідомо',
 			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
 		},
 		3: {
-			'lessonName': 'Економіка',
-			'courseLink': '<a href="https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja" target="_blank">https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja</a>',
+			'lessonName': 'Економіка промисловості',
+			'courseLink': '<a href="https://classroom.google.com/c/NzEwMzMzNjI1NDE3?cjc=554biqx" target="_blank">https://classroom.google.com/c/NzEwMzMzNjI1NDE3?cjc=554biqx</a>',
 			'lessonLink': '<a href="https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09" target="_blank">https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09</a>'
 		}
 	},
@@ -37,51 +37,51 @@ const schedule = {
 		2: {
 			'lessonName': 'Соціальна психологія',
 			'courseLink': '<a href="https://classroom.google.com/c/NzEwMTM5NTY1ODgx?cjc=3u6qgox" target="_blank">https://classroom.google.com/c/NzEwMTM5NTY1ODgx?cjc=3u6qgox</a>',
-			'lessonLink': 'невідомо'
+			'lessonLink': 'немає'
 		},
 		3: {
-			'lessonName': 'РТС',
-			'courseLink': '<a href="https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo" target="_blank">https://classroom.google.com/c/NTg5NTE2MzE5NzA5?cjc=rmuq5oo</a>',
+			'lessonName': 'Радіотехнічні та радіоелектронні системи',
+			'courseLink': '<a href="https://classroom.google.com/c/NzEwMzU2OTI0MjQ2?cjc=dhetudz" target="_blank">https://classroom.google.com/c/NzEwMzU2OTI0MjQ2?cjc=dhetudz</a>',
 			'lessonLink': '<a href="https://meet.google.com/icp-hqmi-non" target="_blank">https://meet.google.com/icp-hqmi-non</a>'
 		},
 		4: {
-			'lessonName': 'невідомо',
-			'courseLink': 'невідомо',
-			'lessonLink': 'невідомо'
+			'lessonName': 'Охорона праці (в галузі)',
+			'courseLink': '<a href="https://classroom.google.com/c/NzEwMjUwNTE2MzIz?cjc=i4v3dyd" target="_blank">https://classroom.google.com/c/NzEwMjUwNTE2MzIz?cjc=i4v3dyd</a>',
+			'lessonLink': '<a href="https://meet.google.com/met-uedi-wko" target="_blank">https://meet.google.com/met-uedi-wko</a>'
 		}
 	},
 	4: {
 		1: {
-			'lessonName': 'невідомо/невідомо',
+			'lessonName': 'Апаратура систем зв\'язку та телебачення/Контроль якості РЕА',
 			'courseLink': 'невідомо/невідомо',
-			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
+			'lessonLink': 'Апаратура систем зв\'язку та телебачення: <a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a><br>Контроль якості РЕА: невідомо'
 		},
 		2: {
-			'lessonName': 'невідомо',
+			'lessonName': 'Кінцеві пристрої доступу до інформації',
 			'courseLink': 'невідомо',
-			'lessonLink': '<a href="https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09" target="_blank">https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09</a>'
+			'lessonLink': 'невідомо'
 		},
 		3: {
-			'lessonName': 'Економіка',
-			'courseLink': '<a href="https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja" target="_blank">https://classroom.google.com/c/NTQ1ODQ1NjI4NDM2?cjc=tlvmkja</a>',
+			'lessonName': 'Економіка промисловості',
+			'courseLink': '<a href="https://classroom.google.com/c/NzEwMzMzNjI1NDE3?cjc=554biqx" target="_blank">https://classroom.google.com/c/NzEwMzMzNjI1NDE3?cjc=554biqx</a>',
 			'lessonLink': '<a href="https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09" target="_blank">https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09</a>'
 		}
 	},
 	5: {
 		2: {
-			'lessonName': 'невідомо',
+			'lessonName': 'Основи комп\'ютерного проектування РЕА',
 			'courseLink': 'невідомо',
 			'lessonLink': 'невідомо'
 		},
 		3: {
-			'lessonName': 'невідомо',
+			'lessonName': 'Конструювання ВЕТ і САПР',
 			'courseLink': 'невідомо',
 			'lessonLink': 'невідомо'
 		},
 		4: {
-			'lessonName': 'невідомо/невідомо',
-			'courseLink': 'невідомо/невідомо',
-			'lessonLink': 'невідомо/невідомо'
+			'lessonName': 'Основи ремонту та регулювання РЕА/Охорона праці (в галузі)',
+			'courseLink': 'Основи ремонту та регулювання РЕА: невідомо<br>Охорона праці (в галузі): <a href="https://classroom.google.com/c/NzEwMjUwNTE2MzIz?cjc=i4v3dyd" target="_blank">https://classroom.google.com/c/NzEwMjUwNTE2MzIz?cjc=i4v3dyd</a>',
+			'lessonLink': 'Основи ремонту та регулювання РЕА: невідомо<br>Охорона праці (в галузі): <a href="https://meet.google.com/met-uedi-wko" target="_blank">https://meet.google.com/met-uedi-wko</a>'
 		}
 	}
 }
