4d379c5b6bd25e360dfecd3606ae80781b49b205
EtbonDeveloper <etbon.development@gmail.com>
2024-09-04 18:26:16 +0300

    fix classes.js

--- js/classes.js
+++ js/classes.js
@@ -1,6 +1,12 @@
 import schedule from './schedule.js';
-import strDayOfWeekObject from './day-of-week.js';
 
+const strDayOfWeekObject = {
+    1: 'Понеділок',
+    2: 'Вівторок',
+    3: 'Середа',
+    4: 'Четвер',
+    5: 'П\'ятниця'
+};
 const timetable = document.querySelector('#timetable');
 timetable.innerHTML = '';
 
