8cdc0c01a689ca3182a5e15fb34b805cfc4df34f
EtbonDeveloper <etbon.development@gmail.com>
2024-09-08 19:52:35 +0300

    change schedule

--- js/schedule.js
+++ js/schedule.js
@@ -1,5 +1,10 @@
 const schedule = {
 	1: {
+		1: {
+			'lessonName': 'Основи ремонту та регулювання РЕА',
+			'courseLink': '<a href="https://distosvitart.gnomio.com/course/view.php?id=22" target="_blank">https://distosvitart.gnomio.com/course/view.php?id=22</a>',
+			'lessonLink': '<a href="https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09" target="_blank">https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09</a>'
+		},
 		2: {
 			'lessonName': 'Радіотехнічні та радіоелектронні системи',
 			'courseLink': '<a href="https://classroom.google.com/c/NzEwMzU2OTI0MjQ2?cjc=dhetudz" target="_blank">https://classroom.google.com/c/NzEwMzU2OTI0MjQ2?cjc=dhetudz</a>',
@@ -7,25 +12,20 @@ const schedule = {
 		},
 		3: {
 			'lessonName': 'Апаратура систем зв\'язку та телебачення',
-			'courseLink': 'невідомо',
-			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
-		},
-		4: {
-			'lessonName': 'Основи комп\'ютерного проектування РЕА/Конструювання ВЕТ і САПР',
-			'courseLink': 'невідомо/невідомо',
-			'lessonLink': 'невідомо/невідомо'
+			'courseLink': '<a href="https://classroom.google.com/c/NzExNjAxMDYzOTY4?cjc=cgjfczk" target="_blank">https://classroom.google.com/c/NzExNjAxMDYzOTY4?cjc=cgjfczk</a>',
+			'lessonLink': '<a href="https://us04web.zoom.us/j/8253570018?pwd=N1NnMXArS21YSGpzeTB2N1pONFFEdz09" target="_blank">https://us04web.zoom.us/j/8253570018?pwd=N1NnMXArS21YSGpzeTB2N1pONFFEdz09</a>'
 		}
 	},
 	2: {
 		1: {
-			'lessonName': 'Контроль якості РЕА',
-			'courseLink': 'невідомо',
-			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
+			'lessonName': 'Основи ремонту та регулювання РЕА/Основи комп\'ютерного проектування РЕА',
+			'courseLink': '<br>Основи ремонту та регулювання РЕА: <a href="https://distosvitart.gnomio.com/course/view.php?id=22" target="_blank">https://distosvitart.gnomio.com/course/view.php?id=22</a><br>Основи комп\'ютерного проектування РЕА: <a href="https://classroom.google.com/c/NzEwMjYwNTExODE4?cjc=yg5tgdh" target="_blank">https://classroom.google.com/c/NzEwMjYwNTExODE4?cjc=yg5tgdh</a>',
+			'lessonLink': '<br>Основи ремонту та регулювання РЕА: <a href="https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09" target="_blank">https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09</a><br>Основи комп\'ютерного проектування РЕА: <a href="https://us05web.zoom.us/j/8454965992?pwd=cnNxV0M3a0lReEljRjlPenRCcC9Rdz09" target="_blank">https://us05web.zoom.us/j/8454965992?pwd=cnNxV0M3a0lReEljRjlPenRCcC9Rdz09</a>'
 		},
 		2: {
-			'lessonName': 'Основи ремонту та регулювання РЕА',
+			'lessonName': 'Контроль якості РЕА',
 			'courseLink': 'невідомо',
-			'lessonLink': '<a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a>'
+			'lessonLink': 'невідомо'
 		},
 		3: {
 			'lessonName': 'Економіка промисловості',
@@ -37,7 +37,7 @@ const schedule = {
 		2: {
 			'lessonName': 'Соціальна психологія',
 			'courseLink': '<a href="https://classroom.google.com/c/NzEwMTM5NTY1ODgx?cjc=3u6qgox" target="_blank">https://classroom.google.com/c/NzEwMTM5NTY1ODgx?cjc=3u6qgox</a>',
-			'lessonLink': 'немає'
+			'lessonLink': '<a href="https://us04web.zoom.us/j/2106470177?pwd=JzvM33IedAHXFHyLr4i3wvgzR7efZS.1" target="_blank">https://us04web.zoom.us/j/2106470177?pwd=JzvM33IedAHXFHyLr4i3wvgzR7efZS.1</a>'
 		},
 		3: {
 			'lessonName': 'Радіотехнічні та радіоелектронні системи',
@@ -51,37 +51,37 @@ const schedule = {
 		}
 	},
 	4: {
-		1: {
-			'lessonName': 'Апаратура систем зв\'язку та телебачення/Контроль якості РЕА',
-			'courseLink': 'невідомо/невідомо',
-			'lessonLink': 'Апаратура систем зв\'язку та телебачення: <a href="https://t.me/+xlyhWoe5MlEyMDhi" target="_blank">https://t.me/+xlyhWoe5MlEyMDhi</a><br>Контроль якості РЕА: невідомо'
-		},
 		2: {
 			'lessonName': 'Кінцеві пристрої доступу до інформації',
-			'courseLink': 'невідомо',
-			'lessonLink': 'невідомо'
+			'courseLink': '<a href="https://distosvitart.gnomio.com/course/view.php?id=11" target="_blank">https://distosvitart.gnomio.com/course/view.php?id=11</a>',
+			'lessonLink': '<a href="https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09" target="_blank">https://us04web.zoom.us/j/3989248377?pwd=WlJlOXZ1dmw1bkZxWlUyd2JGcUM2QT09</a>'
 		},
 		3: {
 			'lessonName': 'Економіка промисловості',
 			'courseLink': '<a href="https://classroom.google.com/c/NzEwMzMzNjI1NDE3?cjc=554biqx" target="_blank">https://classroom.google.com/c/NzEwMzMzNjI1NDE3?cjc=554biqx</a>',
 			'lessonLink': '<a href="https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09" target="_blank">https://us05web.zoom.us/j/86781094480?pwd=WE9Nc3puOFhKVFppWnoxRjhpWG9nUT09</a>'
-		}
+		},
+		4: {
+			'lessonName': 'Контроль якості РЕА/Конструювання ВЕТ і САПР',
+			'courseLink': '<br>Контроль якості РЕА: невідомо<br>Конструювання ВЕТ і САПР: <a href="https://classroom.google.com/c/NzEwMjU1NDAyOTYy?cjc=mrvc4ky" target="_blank">https://classroom.google.com/c/NzEwMjU1NDAyOTYy?cjc=mrvc4ky</a>',
+			'lessonLink': '<br>Контроль якості РЕА: невідомо<br>Конструювання ВЕТ і САПР: <a href="https://us05web.zoom.us/j/8454965992?pwd=cnNxV0M3a0lReEljRjlPenRCcC9Rdz09" target="_blank">https://us05web.zoom.us/j/8454965992?pwd=cnNxV0M3a0lReEljRjlPenRCcC9Rdz09</a>'
+		},
 	},
 	5: {
 		2: {
 			'lessonName': 'Основи комп\'ютерного проектування РЕА',
-			'courseLink': 'невідомо',
-			'lessonLink': 'невідомо'
+			'courseLink': '<a href="https://classroom.google.com/c/NzEwMjYwNTExODE4?cjc=yg5tgdh" target="_blank">https://classroom.google.com/c/NzEwMjYwNTExODE4?cjc=yg5tgdh</a>',
+			'lessonLink': '<a href="https://us05web.zoom.us/j/8454965992?pwd=cnNxV0M3a0lReEljRjlPenRCcC9Rdz09" target="_blank">https://us05web.zoom.us/j/8454965992?pwd=cnNxV0M3a0lReEljRjlPenRCcC9Rdz09</a>'
 		},
 		3: {
 			'lessonName': 'Конструювання ВЕТ і САПР',
-			'courseLink': 'невідомо',
-			'lessonLink': 'невідомо'
+			'courseLink': '<a href="https://classroom.google.com/c/NzEwMjU1NDAyOTYy?cjc=mrvc4ky" target="_blank">https://classroom.google.com/c/NzEwMjU1NDAyOTYy?cjc=mrvc4ky</a>',
+			'lessonLink': '<a href="https://us05web.zoom.us/j/8454965992?pwd=cnNxV0M3a0lReEljRjlPenRCcC9Rdz09" target="_blank">https://us05web.zoom.us/j/8454965992?pwd=cnNxV0M3a0lReEljRjlPenRCcC9Rdz09</a>'
 		},
 		4: {
-			'lessonName': 'Основи ремонту та регулювання РЕА/Охорона праці (в галузі)',
-			'courseLink': 'Основи ремонту та регулювання РЕА: невідомо<br>Охорона праці (в галузі): <a href="https://classroom.google.com/c/NzEwMjUwNTE2MzIz?cjc=i4v3dyd" target="_blank">https://classroom.google.com/c/NzEwMjUwNTE2MzIz?cjc=i4v3dyd</a>',
-			'lessonLink': 'Основи ремонту та регулювання РЕА: невідомо<br>Охорона праці (в галузі): <a href="https://meet.google.com/met-uedi-wko" target="_blank">https://meet.google.com/met-uedi-wko</a>'
+			'lessonName': 'Апаратура систем зв\'язку та телебачення/Охорона праці (в галузі)',
+			'courseLink': '<br>Апаратура систем зв\'язку та телебачення: <a href="https://classroom.google.com/c/NzExNjAxMDYzOTY4?cjc=cgjfczk" target="_blank">https://classroom.google.com/c/NzExNjAxMDYzOTY4?cjc=cgjfczk</a><br>Охорона праці (в галузі): <a href="https://classroom.google.com/c/NzEwMjUwNTE2MzIz?cjc=i4v3dyd" target="_blank">https://classroom.google.com/c/NzEwMjUwNTE2MzIz?cjc=i4v3dyd</a>',
+			'lessonLink': '<br>Апаратура систем зв\'язку та телебачення: <a href="https://us04web.zoom.us/j/8253570018?pwd=N1NnMXArS21YSGpzeTB2N1pONFFEdz09" target="_blank">https://us04web.zoom.us/j/8253570018?pwd=N1NnMXArS21YSGpzeTB2N1pONFFEdz09</a><br>Охорона праці (в галузі): <a href="https://meet.google.com/met-uedi-wko" target="_blank">https://meet.google.com/met-uedi-wko</a>'
 		}
 	}
 }
