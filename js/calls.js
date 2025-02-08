61dbf50e9d43cfefaf593847fe1764f28d2c3a3e
EtbonDeveloper <etbon.development@gmail.com>
2025-02-08 15:56:04 +0200

    change the displayed content

--- js/calls.js
+++
@@ -1,9 +0,0 @@
-import calls from './calls-object.js';
-
-const callsElement = document.querySelector('#calls');
-
-for (const numberOfLesson in calls) {
-    const callElement = document.createElement('p');
-    callElement.innerText = `${numberOfLesson} пара: ${calls[numberOfLesson].startTime} - ${calls[numberOfLesson].endTime}`;
-    callsElement.append(callElement);
-}
